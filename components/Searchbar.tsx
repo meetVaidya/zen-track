fb3f62b3cc6bb8480198d26bc267838eda13aaff
meext <vaidyameet2004@gmail.com>
2024-04-06 08:34:28 +0530

    Refactor Searchbar component to include form submission functionality

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,7 +1,20 @@
+'use client';
+
 import React from 'react';
 
 const Searchbar = () => {
-    return <form className="flex flex-wrap gap-4 mt-12"></form>;
+    const handleSubmit = () => {};
+
+    return (
+        <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Enter product link..."
+                className="searchbar-input"
+            />
+            <button type='submit' className='searchbar-btn'></button>
+        </form>
+    );
 };
 
 export default Searchbar;
